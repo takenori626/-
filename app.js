a0a08d4089c66857da13383dcc05cbc94d10f2cb
takenori626 <takenori626@gmail.com>
2025-06-26 07:28:02 +0000

    2506261627

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+const { memo } = require("react");
+
 //ページ読み込み時に保存済みのメモを表示
 const memoArea = document.getElementById("memo");
+const savedMemo = localStorage.getItem("memo");
+if (savedMemo) {
+    memoArea.value = savedMemo;
+}
+//保存ボタンを押したときの処理
+document.getElementById("save").addEventListener("click",() => {
+    localStorage.setItem
+}
